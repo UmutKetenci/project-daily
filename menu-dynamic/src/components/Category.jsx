7706c46b7ffc4d39d897a0f956ce2136e04d52e9
UmutKetenci <44112454+UmutKetenci@users.noreply.github.com>
2022-01-25 15:20:21 +0300

    Update Category.jsx
    removed unused class name.

--- menu-dynamic/src/components/Category.jsx
+++ menu-dynamic/src/components/Category.jsx
@@ -5,7 +5,7 @@ const Category = ({ filterItems }) => {
   const allCategories = ["all", ...new Set(data.map((item) => item.category))];
   console.log(allCategories);
   return (
-    <div className="categoriiii">
+    <div>
       {allCategories.map((category) => (
         <button onClick={() => filterItems(category)}>{category}</button>
       ))}
