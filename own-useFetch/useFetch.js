854f736f8c945bc4d738950235bb497b99c52e61
UmutKetenci <44112454+UmutKetenci@users.noreply.github.com>
2022-01-31 19:04:03 +0300

    Add files via upload

---
+++ own-useFetch/useFetch.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const useFetch = () => {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch("your link goes here");
+    const data = await response.json();
+    setData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return { data, loading };
+};
