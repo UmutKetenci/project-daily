e691acad1e3ac09e1b15bb152969cf9a8ea039b6
UmutKetenci <44112454+UmutKetenci@users.noreply.github.com>
2022-02-18 05:09:58 +0300

    Update App.js

--- chronometer/src/App.js
+++ chronometer/src/App.js
@@ -4,7 +4,7 @@ import Clock from "../src/components/Clock";
 function App() {
   return (
     <section className="section">
-      <div className="center">
+      <div className="center a">
         <Clock></Clock>
       </div>
     </section>
